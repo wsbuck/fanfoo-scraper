3375af7238d8c1dd6d1d831b86d1e9b532655f5a
William Buck <wsbuckiii@gmail.com>
2019-09-27 17:40:28 -0700

    added yahoo fantasy projections

--- src/scrape.py
+++ src/scrape.py
@@ -3,14 +3,64 @@ import json
 import pandas as pd
 
 from bs4 import BeautifulSoup
+from requests.models import PreparedRequest
+
+def name_cleanup(name):
+    name = name.lower()
+    name = name.replace('notes', '').replace('note', '')
+    name = name.replace('no', '')
+    name = name.replace('player', '').replace('players', '')
+    name = name.replace('\n', '').replace('jr.', '').replace('sr.', '')
+    name = name.replace('iii', '').replace('new', '')
+    name = name.strip()
+    name = name.split()
+    return name[0] + ' ' + name[1]
 
 
-def scrapeNFL(season, week):
+def scrapeYahoo(season, week, position):
+    url = 'https://football.fantasysports.yahoo.com/f1/47241/players'
+    drop_cols = [
+        0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 17, 19, 20, 24,
+        25, 30, 31, 32, 33, 34, 35, 36, 37
+    ]
+    col_names = [
+        'name', 'pass_att', 'pass_comp', 'pass_yds', 'pass_td', 'pass_int',
+        'pass_sack', 'rush_att', 'rush_yds', 'rush_td', 'rec_tgt', 'rec_rec',
+        'rec_yds', 'rec_td'
+    ]
+    all_players = pd.DataFrame(columns=col_names)
+
+    for count in range(0, 300, 25):
+        querystring = {
+            'sort': 'PTS',
+            'sdir': '1',
+            'status': 'A',
+            'pos': '{}'.format(position),
+            'stat1': 'S_PW_{}'.format(week),
+            'jsenabled': '0',
+            'count': '{}'.format(count)
+        }
+        req = PreparedRequest()
+        req.prepare_url(url, querystring)
+        tables = pd.read_html(req.url)
+        players = tables[1]
+        players.drop(players.columns[drop_cols], axis=1, inplace=True)
+        players.columns = col_names
+        players['name'] = players['name'].apply(name_cleanup)
+        if len(players) == 0:
+            break
+        all_players = all_players.append(players, ignore_index=True)
+    
+    return all_players
+
+
+def scrapeNFL(season, week, position):
     url = "https://api.fantasy.nfl.com/v1/players/stats"
     querystring = {
-        "statType": "seasonProjectedStats",
+        "statType": "weekProjectedStats",
         "week": "{}".format(week),
         "season": "{}".format(season),
+        "position": "{}".format(position),
         "format": "json"
     }
     headers = {
@@ -61,7 +111,8 @@ def scrapeNFL(season, week):
 
 
 def main():
-    print(scrapeNFL(2019, 0).head())
+    # print(scrapeNFL(2019, 0).head())
+    print(scrapeYahoo('2019', '4', 'QB'))
 
 
 if __name__ == "__main__":
